8a07adf42c171877e0b2cec2e3d4c39b158a1667
Roman Parshin <rparshin@globalrhapsody.com>
2016-03-17 17:36:35 +0100

    255_easy_playing_with_light: version 1 is ready, but too slow, ~ 15 sec.

--- 255_easy_playing_with_light.c
+++ 255_easy_playing_with_light.c
@@ -5,13 +5,20 @@
 const unsigned short size = 8 * sizeof(long);
 const unsigned long mask = ~0;
 
+short debug = 0;
+
+unsigned long invert(unsigned long num, unsigned short p, unsigned short n);
+
 int main()
 {
 	// Bitwise implementation.
 	// One long number is 64 (usually) bits, or 64 bulbs 
 	unsigned int bulbs_num, bulbs_min, bulbs_max;
-	unsigned short longs_num, longs_min, longs_max;
+	unsigned int longs_num, longs_min, longs_max;
 	unsigned short bits_min, bits_max;
+	int i;
+	unsigned short f, t;
+	unsigned long res;
 
 	// Get bulbs number;
 	scanf("%d", &bulbs_num);
@@ -21,14 +28,17 @@ int main()
 	unsigned long* data = (unsigned long*) malloc(longs_num * sizeof(long));
 
 	// Debug output
-	printf("size=%i, bulbs_num=%i, longs_num=%i\n",
-		size, bulbs_num, longs_num);
+	if (debug)
+		printf("size=%i, bulbs_num=%i, longs_num=%i\n",
+			size, bulbs_num, longs_num);
 
-	*(data+0) = 0;
+	for (i = 0; i < longs_num; i++)
+		*(data+i) = 0;
 
 	// Read number pairs
 	while(scanf("%d", &bulbs_min) != EOF && scanf("%d", &bulbs_max) != EOF)
 	{
+
 		if (bulbs_min > bulbs_max) {
 			bulbs_min += bulbs_max;
 			bulbs_max = bulbs_min - bulbs_max;
@@ -39,19 +49,79 @@ int main()
 		longs_min = bulbs_min / size;
 		longs_max = bulbs_max / size;
 
-		// Debug output
-		printf("bulbs_min=%i, bulbs_max=%i\n", bulbs_min, bulbs_max);
-		printf("longs_min=%i, longs_max=%i\n", longs_min, longs_max);
+		//// Debug output
+		//if (debug) {
+		//	printf("bulbs_min=%i, bulbs_max=%i\n", bulbs_min, bulbs_max);
+		//	printf("longs_min=%i, longs_max=%i\n", longs_min, longs_max);
+		//}
 
 		// Invert
 		if (longs_min == longs_max) {
-			unsigned short p = bulbs_max % size;
-			unsigned short n = bulbs_max - bulbs_min + 1;
-			*(data+0) = n == size ? *(data+0) ^ mask
-			                      : *(data+0) ^ (~(mask << n) << (p - n + 1));
-			printf("res=%lu\n", *(data+0));
+			
+			// Invert bytes in one word from F to T
+			f = bulbs_min % size;
+			t = bulbs_max % size;
+			*(data+longs_min) = invert(*(data+longs_min), f, t);
+			
+			// Debut output
+			if (debug)
+				printf("res%i=%lu / f=%i t=%i\n",
+					longs_min, *(data+longs_min), f, t);
 		}
+
+		else {
+
+			f = bulbs_min % size;
+			t = bulbs_max % size;
+
+			// Invert last F bytes in longs_min
+			*(data+longs_min) = invert(*(data+longs_min), f, size-1);
+
+			//// Debug output
+			//if (debug)
+			//	printf("res%i=%lu / f=%i t=%i\n",
+			//		longs_min, *(data+longs_min), f, size-1);
+
+			// Invert all bites between longs_min and longs_max
+			if (longs_max - longs_min > 1) {
+				for (i = longs_min + 1; i < longs_max; i++);
+					//*(data+i) = *(data+i) ^ mask;
+					
+					//// Debug output
+					//if (debug)
+					//	printf("res%i=%lu\n", i, *(data+i));
+			}
+
+			// Invers first N bytes in longs_max
+			*(data+longs_max) = invert(*(data+longs_max), 0, t);
+
+			//// Debug output
+			//if (debug)
+			//	printf("res%i=%lu / p=%i n=%i\n",
+			//		longs_max, *(data+longs_max), 0, t);
+		}
+
 	}
 
+	printf("done...%lu\n", tmp);
+
+	// Done, calc 1-bits in all numbers
+	res = 0;
+	for (i = 0; i < longs_num; i++)
+		while (*(data+i) > 0) {
+			if ((*(data+i) & 1) == 1)
+				res++;
+			*(data+i) >>= 1;
+		}
+
+	printf("res=%lu\n", res);
+
 	return 1;
+}
+
+unsigned long invert(unsigned long num, unsigned short from, unsigned short to)
+{
+	return to - from == size - 1
+		? num ^ mask
+		: num ^ (~(mask << (to - from + 1)) << from);
 }
